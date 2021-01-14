ecaef484121060014e8f29929aa3cd5b0eb90731
dengninghai <2640559435@qq.com>
2021-01-14 13:54:50 +0800

    add Link

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import icon from '../../assets/image/logo.png';
 import { DownOutlined } from '@ant-design/icons';
 import './index.less';
@@ -7,18 +8,22 @@ const MMenu: React.FC = () => {
 
   return (
     <div className='menu'>
-      <div className='header'>
+      {/* <div className='header'>
         <img className='logo' src={icon} alt="" />
         <div className='title'>融媒体</div>
-      </div>
+      </div> */}
       <div className='menuInfo'>
         <div className='itemList'>
-          <div className='item-title'>
-            demo1 <DownOutlined />
-          </div>
-          <div className='item'>aaaaa</div>
-          <div className='item'>bbbbb</div>
-          <div className='item'>ccccc</div>
+          <div className='item-title'>demo1 <DownOutlined /></div>
+          <Link to='/demo1'>
+            <div className='item'>aaaaa</div>
+          </Link>
+          <Link to='/demo2'>
+            <div className='item'>bbbbb</div>
+          </Link>
+          <Link to='/demo2/demo3'>
+            <div className='item'>ccccc</div>
+          </Link>
         </div>
         <div className='itemList'>
           <div className='item-title'>demo2 <DownOutlined /></div>
@@ -27,7 +32,7 @@ const MMenu: React.FC = () => {
           <div className='item'>ccccc</div>
         </div>
         <div className='itemList'>
-          <div className='item-title'>demo3</div>
+          <div className='item-title'>demo3 <DownOutlined /></div>
           <div className='item'>aaaaa</div>
           <div className='item'>bbbbb</div>
           <div className='item'>ccccc</div>
